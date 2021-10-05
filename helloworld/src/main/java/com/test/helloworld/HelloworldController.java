cf6b76a90780359e185b8a9702e9551bf0275f51
Manul Madara <2018is047@stu.ucsc.cmb.ac.lk>
2021-10-05 08:54:59 +0530

    Add files via upload

---
+++ helloworld/src/main/java/com/test/helloworld/HelloworldController.java
@@ -0,0 +1,12 @@
+package com.test.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloworldController {
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
